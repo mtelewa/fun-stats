8cfb865a1272b1c1416a01cb15b8e6b4d2d7b866
Mohamed Tarek <m.tarek@live.co.uk>
2024-04-20 12:54:23 +0000

    put the eventlistener function inside the larger event listener function - the DOM content has to be loaded first

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,18 @@
 const values = [1, 3, 5, 10, 20, 50, 100];
 
 
+/* Wait for the DOM to finish loading before running the quiz
+Add event listener to the user input*/
 
-function printValue(event) {
+document.addEventListener("DOMContentLoaded", function() {
+
     let slider = document.getElementById('slider');
     let output = document.getElementById('chosen-value');
-    output.innerHTML = `Your choice: ${values[slider.value]}`;
-}
+    output.innerHTML = `Your choice: ${values[slider.value]}`
+    slider.addEventListener('input', function() {
+        output.innerHTML = `Your choice: ${values[slider.value]}`;
+    })
+    
 
 
-
-
-slider.addEventListener('input', printValue)
+})
