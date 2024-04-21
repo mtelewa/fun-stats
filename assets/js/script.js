39e1835fda3ffa5783c3dcff3b5df366bb6b2e8c
Mohamed Tarek <m.tarek@live.co.uk>
2024-04-21 12:47:46 +0000

    add event listener to the slider such that the enter button submits the answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,35 @@
 
 // Questions database (ideally this would be imported)
 
-questionsDB = [
-    { category : 'science',
+const questionsDB = [
+    { id : 0,
+      category : 'science',
       question : `How many cells in the human body? <br> (in millions)`,
       answerArray : [10, 50, 100, 1000, 10000, 40000, 100000],
       correctAnswerIndex : 5
     },
-    { category : 'science',
+    { id : 1,
+      category : 'science',
       question : `How many sand grains are on Earth? <br> (in sextillions)`,
       answerArray : [1, 3, 5, 7, 10, 50, 100],
       correctAnswerIndex : 3
     },
-    { category : 'sports',
+    { id : 2,
+      category : 'sports',
       question : `How many times did Real Madrid win UEFA champions league?`,
       answerArray : [1, 3, 5, 7, 10, 20, 50],
       correctAnswerIndex : 3
     }, 
-    { category : 'history',
+    { id : 3,
+      category : 'history',
       question : `How many paintings did Picasso produce?`,
-      answerArray : [1, 3, 5, 7, 10, 20, 50],
+      answerArray : [3, 4, 9, 12, 17, 20, 50],
+      correctAnswerIndex : 3
+    },
+    { id : 4,
+      category : 'arts',
+      question : `How many paintings did Picasso produce?`,
+      answerArray : [3, 4, 9, 12, 17, 20, 50],
       correctAnswerIndex : 3
     }
 ]
@@ -32,27 +42,10 @@ Add event listener to the user input*/
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Print min, max and user-chosen values
-    let slider = document.getElementById('slider');
-    let output = document.getElementById('chosen-value');
-    let minValue = document.getElementById('min-value');
-    let maxValue = document.getElementById('max-value');
+    displayQuestion()
+    displayAnswer()
+    fetchUserAnswer()
 
-    // home page does not have the slider object and so `slider` will be null
-    if (slider != null) {
-      // Default value is printed in the output paragraph unless changed by the user
-      output.innerHTML = `Your choice: ${questionsDB[0]['answerArray'][slider.value]}`;
-      minValue.innerHTML = questionsDB[0]['answerArray'][0].toLocaleString(); // write it in a readable format
-      maxValue.innerHTML = questionsDB[0]['answerArray'][6].toLocaleString(); // write it in a readable format
-
-      slider.addEventListener('input', function() {
-          output.innerHTML = `Your choice: ${questionsDB[0]['answerArray'][slider.value].toLocaleString()}`;
-      
-      })
-
-      displayQuestion()
-    
-    }
 
 })
 
@@ -107,15 +100,72 @@ function fetchQuestions() {
 
 }
 
+
 /**
  * prints the question to the screen based on an integer random number from 1 to 30
  */
 function displayQuestion() {
-    let randomNum = Math.floor(Math.random() * 2);
+    let randomNum = Math.floor(Math.random() * 0);
     let question = document.getElementById('question');
+
     question.innerHTML = `<h2> ${fetchQuestions()[randomNum]['question']} </h2>`;
+
+    return randomNum
 }
 
+
+/**
+ * prints the answers range to the screen based on the random number created from displayQuestion function
+ */
+function displayAnswer() {
+    // Min, max and user-chosen values
+    let slider = document.getElementById('slider');
+    // focus on slider when page is loaded
+    slider.focus();
+
+    let outChosen = document.getElementById('chosen-value');
+    let outMin = document.getElementById('min-value');
+    let outMax = document.getElementById('max-value');
+
+    let randomNumberGenerated = displayQuestion();
+    let answersRange = fetchQuestions()[randomNumberGenerated]['answerArray'];
+
+    // home page does not have the slider object and so `slider` will be null
+    if (slider != null) {
+      // default value is printed in the output paragraph unless changed by the user
+      outChosen.innerHTML = `Your choice: ${answersRange[slider.value]}`;
+      outMin.innerHTML = answersRange[0].toLocaleString(); // write it in a readable format
+      outMax.innerHTML = answersRange[6].toLocaleString(); // write it in a readable format
+
+      slider.addEventListener('input', function() {
+          outChosen.innerHTML = `Your choice: ${answersRange[slider.value].toLocaleString()}`;
+      
+      })
+
+      let userAnswerIndex = slider.value
+
+      slider.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+          console.log('submit pressed')
+        }
+      })
+      
+    return userAnswerIndex
+    }
+
+}
+
+
+function fetchUserAnswer() {
+    let submitButon = document.getElementById('submit-button')
+    
+    submitButon.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        console.log('submit pressed')
+      }
+    })
+    
+}
 
 
 
